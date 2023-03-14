aec831ffbf96bc80483772baea369cafdf9cabfc
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2023-03-14 11:05:15 +0100

    vulkan: adjusted bootup to new reinit future/async support

--- space-client/src/bootup.rs
+++ space-client/src/bootup.rs
@@ -1,12 +1,11 @@
 use std::sync::Arc;
 
 use clap::Parser;
-use futures::executor::block_on;
 use vulkano::swapchain::Surface;
 use vulkano_win::create_surface_from_winit;
 use winit::window::WindowBuilder;
 
-use space_engine::{reinit, reinit_no_restart};
+use space_engine::{reinit, reinit_future, reinit_map, reinit_no_restart};
 use space_engine::vulkan::init::{init, Init, Plugin};
 use space_engine::vulkan::plugins::renderdoc_layer_plugin::RenderdocLayerPlugin;
 use space_engine::vulkan::plugins::standard_validation_layer_plugin::StandardValidationLayerPlugin;
@@ -20,22 +19,22 @@ use crate::vulkan::{Queues, SpaceQueueAllocator};
 
 reinit_no_restart!(pub WINDOW_SYSTEM: bool = true);
 reinit_no_restart!(pub CLI: Cli = Cli::parse());
-reinit!(pub RENDERDOC_ENABLE: bool = (CLI: Cli) => |cli, _| cli.renderdoc);
-reinit!(pub VALIDATION_LAYER: bool = (CLI: Cli) => |cli, _| cli.validation_layer);
+reinit_map!(pub RENDERDOC_ENABLE: bool = (CLI: Cli) => |cli, _| cli.renderdoc);
+reinit_map!(pub VALIDATION_LAYER: bool = (CLI: Cli) => |cli, _| cli.validation_layer);
 reinit!(pub VULKAN_INIT: Init<Queues> = (VALIDATION_LAYER: bool, RENDERDOC_ENABLE: bool, WINDOW_SYSTEM: bool) =>
 	|validation_layer, renderdoc_enable, window_system, _| {
 		let mut plugins: Vec<&mut dyn Plugin> = vec![];
 
 		let mut standard_validation_plugin = StandardValidationLayerPlugin;
-		if **validation_layer {
+		if *validation_layer {
 			plugins.push(&mut standard_validation_plugin);
 		}
 		let mut renderdoc_plugin = RenderdocLayerPlugin;
-		if **renderdoc_enable {
+		if *renderdoc_enable {
 			plugins.push(&mut renderdoc_plugin);
 		}
 		let mut window_plugin = WindowPlugin;
-		if **window_system {
+		if *window_system {
 			plugins.push(&mut window_plugin);
 		}
 
@@ -46,10 +45,9 @@ reinit!(pub VULKAN_INIT: Init<Queues> = (VALIDATION_LAYER: bool, RENDERDOC_ENABL
 
 // TODO WindowBuilder is not Send, needs separate config type
 // reinit!(WINDOW_CONFIG: Mutex<WindowBuilder> = () => |_| Mutex::new(WindowBuilder::new()));
-// FIXME these should not be using block_on() but instead just delay the internal call to constructed()
-reinit!(pub WINDOW: WindowRef = () => |_| block_on(run_on_event_loop(|event_loop| WindowRef::new(WindowBuilder::new().build(event_loop).unwrap()))));
-reinit!(pub SURFACE: Arc<Surface> = (WINDOW: WindowRef, VULKAN_INIT: Init<Queues>) => |window, init, _| {
-	let instance = init.instance.clone();
-	let window = (**window).clone();
-	block_on(run_on_event_loop(move |event_loop| create_surface_from_winit(window.get_arc(event_loop).clone(), instance).unwrap()))
+reinit_future!(pub WINDOW: WindowRef = () => |_| {
+	run_on_event_loop(|event_loop|WindowRef::new(WindowBuilder::new().build(event_loop).unwrap()))
+});
+reinit_future!(pub SURFACE: Arc<Surface> = (WINDOW: WindowRef, VULKAN_INIT: Init<Queues>) => |window, init, _| {
+	run_on_event_loop(move |event_loop| create_surface_from_winit(window.get_arc(event_loop).clone(), init.instance.clone()).unwrap())
 });
--- space-client/src/main.rs
+++ space-client/src/main.rs
@@ -9,7 +9,6 @@ use vulkano::swapchain::Surface;
 use space_client::bootup::SURFACE;
 use space_engine::reinit;
 use space_engine::reinit::Target;
-use space_engine::reinit::State::Initialized;
 use space_engine::vulkan::window::event_loop::{event_loop_init, run_on_event_loop};
 
 struct Main {}
@@ -21,7 +20,6 @@ reinit!(MAIN: Main = (SURFACE: Arc<Surface>) => |_, _| Main {});
 fn main() {
 	event_loop_init(true, |_rx| {
 		let _need = MAIN.need();
-		MAIN.assert_state(Initialized);
 
 		let event_loop = run_on_event_loop(|_| {
 			assert_eq!(current().name().unwrap(), "main");
