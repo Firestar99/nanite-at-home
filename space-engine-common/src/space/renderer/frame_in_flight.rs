db2296c65216361b2d80720f85ba7dc4615f4662
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2023-09-05 19:26:08 +0200

    frame_in_flight: mod docs

--- space-engine-common/src/space/renderer/frame_in_flight.rs
+++ space-engine-common/src/space/renderer/frame_in_flight.rs
@@ -1,3 +1,31 @@
+//! # Frame in flight
+//! The Frame in flight system consists out of 3 main components:
+//! * `SeedInFlight`: The seed is the configuration of the Frame in flight system and ensures different seeds are not mixed or matched.
+//! To construct it you must pass the amount of frames that may be in flight at maximum, so that other systems can allocate enough resources
+//! to support that many frames in flight. The maximum may also not exceed the `FRAMES_LIMIT` of 3, see Efficiency for why.
+//! * `FrameInFlight`: A Frame in flight is effectively the index of the frame that is currently in flight. It is constructed via the
+//! `new()` from a seed and the index that it should represent, which is marked unsafe as one needs to ensure two frames in flight with
+//! the same index are never executing at the same time.
+//! * `ResourceInFlight`: A `ResourceInFlight` is a resource that is allocated once per frame that may be in flight at the same time.
+//! `FrameInFlight` may be used to index into the `ResourceInFlight` to get the resource to be used for that particular frame. The
+//! Resources themselves are stored contiguously inside the `ResourceInFlight` type, and not separately on heap.
+//!
+//! # Efficiency and Safety
+//! First each component stores the seed it was constructed with and checks that it is accessed with the seed it was constructed with,
+//! so that no mixing of different seeds may occur. This allows the following conditions to always be true:
+//!
+//! * `SeedInFlight.frames_in_flight <= FRAMES_LIMIT` checked during Seed construction
+//! * `FrameInFlight.index < SeedInFlight.frames_in_flight` checked during `FrameInFlight` construction
+//!
+//! This has two main advantages:
+//! * As indices are always checked during construction, indexing of a `ResourceInFlight` with a `FrameInFlight` may happen without
+//! checking that the index is in bound.
+//! * `ResourceInFlight` has array of it's resource instead of having to heap allocate it's data and thus introduce an indirection.
+//! This array has a capacity of `FRAMES_LIMIT` and is the main factor in the limit, as raising the limit will inherently increase
+//! the size of all `ResourceInFlight` structs. If the seed uses less than `FRAMES_LIMIT` maximum frames in flight, the remaining
+//! slots in the array will remain uninitialized, but still contribute to the size of `ResourceInFlight`.
+//!
+
 use core::mem::{MaybeUninit, size_of};
 use core::ops::{Index, IndexMut};
 
@@ -5,6 +33,7 @@ use static_assertions::const_assert_eq;
 
 pub const FRAMES_LIMIT: u32 = 3;
 
+/// The index of a frame that is in flight. See [mod](self) for docs.
 #[derive(Copy, Clone)]
 #[cfg_attr(not(target_arch = "spirv"), derive(Debug))]
 #[repr(C)]
@@ -37,10 +66,7 @@ impl From<FrameInFlight> for u32 {
 	}
 }
 
-/// A Seed is a frame in flight "configuration". `ResourceInFlight` created with a seed may only be indexed using a FrameInFlight created from the same seed.
-/// All members are private and must be accessed though getters as mutating them is unsafe.
-///
-/// Impl-Note: `frames_in_flight` could only be represented by 2 bits and seed get more bits, but I have yet to find a good bitfield crate that works with rust-gpu.
+/// The seed is the configuration of the Frame in flight system and ensures different seeds are not mixed or matched. See [mod](self) for docs.
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
 #[repr(C)]
 pub struct SeedInFlight {
@@ -89,6 +115,7 @@ impl SeedInFlight {
 }
 
 
+/// A `ResourceInFlight` is a resource that is allocated once per frame that may be in flight at the same time. See [mod](self) for docs.
 #[derive(Debug)]
 #[repr(C)]
 pub struct ResourceInFlight<T> {
