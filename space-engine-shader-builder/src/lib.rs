33cbe0570a46cfa6b0cbb67b447d35598ff445bd
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-02-28 12:29:41 +0100

    project: merge shader build scripts into space-engine-shader-builder crate

---
+++ space-engine-shader-builder/src/lib.rs
@@ -0,0 +1,8 @@
+pub use shader_symbols_builder;
+pub use shader_symbols_builder::spirv_builder;
+use shader_symbols_builder::spirv_builder::Capability;
+use shader_symbols_builder::ShaderSymbolsBuilder;
+
+pub fn shader_symbols_builder_configured_for_space_engine(shader_crate: &str) -> ShaderSymbolsBuilder {
+	ShaderSymbolsBuilder::new(shader_crate, "spirv-unknown-vulkan1.2").capability(Capability::RuntimeDescriptorArray)
+}
