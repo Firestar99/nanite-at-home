e89e8db1cc2bb098361d45e53884f2195d2d4a2b
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-06-27 15:56:30 +0200

    renderer: preprocess meshlets in build.rs and load them from file

--- asteroids/src/sample_scene/mod.rs
+++ asteroids/src/sample_scene/mod.rs
@@ -4,9 +4,12 @@ use space_asset::meshlet::instance::MeshletInstance;
 use space_asset::meshlet::mesh::{MeshletData, MeshletMesh};
 use space_asset::meshlet::mesh2instance::{MeshletMesh2Instance, MeshletMesh2InstanceCpu};
 use space_asset::meshlet::offset::MeshletOffset;
-use space_asset::meshlet::scene::MeshletSceneCpu;
+use space_asset::meshlet::scene::{LoadedMeshletSceneDisk, MeshletSceneCpu};
 use space_asset::meshlet::vertex::MeshletDrawVertex;
+use space_asset::uploader::Uploader;
 use space_engine::space::Init;
+use std::fs::File;
+use std::io;
 use std::iter::repeat;
 use std::sync::Arc;
 use vulkano::buffer::{BufferCreateInfo, BufferUsage};
@@ -15,9 +18,29 @@ use vulkano_bindless::descriptor::RCDescExt;
 
 #[profiling::function]
 pub async fn load_scene(init: &Arc<Init>) -> Vec<Arc<MeshletSceneCpu>> {
+	let scene = upload_test_scene(init);
+
 	let mut out = Vec::new();
 	out.push(upload_test_mesh(init));
+	out.push(scene.await.unwrap());
 	out
+}
+
+#[profiling::function]
+async fn upload_test_scene(init: &Arc<Init>) -> io::Result<Arc<MeshletSceneCpu>> {
+	let disk = unsafe {
+		let path = env!("TestScenePath");
+		let file = File::open(path)?;
+		LoadedMeshletSceneDisk::deserialize_decompress(file)?
+	};
+	let uploader = Uploader {
+		bindless: init.bindless.clone(),
+		memory_allocator: init.memory_allocator.clone(),
+		cmd_allocator: init.cmd_buffer_allocator.clone(),
+		transfer_queue: init.queues.client.transfer.clone(),
+	};
+	let cpu = disk.deserialize().upload(&uploader).await.unwrap();
+	Ok(Arc::new(cpu))
 }
 
 #[profiling::function]
@@ -117,7 +140,7 @@ fn upload_test_mesh(init: &Arc<Init>) -> Arc<MeshletSceneCpu> {
 						(0..4).map(move |z| {
 							MeshletInstance::new(Affine3A::from_translation(vec3(
 								x as f32 * 31.,
-								y as f32 * 31.,
+								y as f32 * 31. + 4.,
 								z as f32 * 4.,
 							)))
 						})
