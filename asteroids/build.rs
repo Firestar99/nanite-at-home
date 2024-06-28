6ff698a181aaaf0ba13855794af70b4f51daa296
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-06-28 15:49:26 +0200

    asteroids: rebuild gltf only if it changed

--- asteroids/build.rs
+++ asteroids/build.rs
@@ -16,10 +16,15 @@ const GLTF_MODEL_PATH: &str = concat!(
 // const GLTF_MODEL_PATH: &str = "../../models/glTF-Sample-Assets/Models/Sponza/glTF/Sponza.gltf";
 
 fn main() -> Result<(), Box<dyn Error>> {
+	// Rerun build script if dir containing gltf has changed. That is technically not sufficient, as gltf may refer to
+	// files outside the parent directory, but that is heavily discouraged.
+	let in_path = PathBuf::from(GLTF_MODEL_PATH);
+	println!("cargo:rerun-if-changed={}", in_path.parent().unwrap().to_str().unwrap());
+
 	let out_path = Path::new(&env::var("OUT_DIR").unwrap()).join("TestScene.bin.zstd");
 	println!("cargo:rustc-env=TestScenePath={}", out_path.to_str().unwrap());
 
-	let gltf = Gltf::open(PathBuf::from(GLTF_MODEL_PATH))?;
+	let gltf = Gltf::open(in_path)?;
 	let disk = block_on(gltf.process())?;
 	disk.serialize_compress_to(File::create(out_path)?)?;
 
