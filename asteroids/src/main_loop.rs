78adf723f36bf7c2011b30cefa48e128ea787b17
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-07-14 12:57:47 +0200

    asteroids: removed smol threadpool remnant

--- asteroids/src/main_loop.rs
+++ asteroids/src/main_loop.rs
@@ -15,7 +15,6 @@ use space_engine::window::window_ref::WindowRef;
 use space_engine_shader::renderer::camera::Camera;
 use space_engine_shader::renderer::frame_data::FrameData;
 use std::f32::consts::PI;
-use std::num::NonZeroUsize;
 use std::sync::mpsc::Receiver;
 use vulkano::shader::ShaderStages;
 use vulkano::sync::GpuFuture;
@@ -41,13 +40,6 @@ pub async fn run(event_loop: EventLoopExecutor, inputs: Receiver<Event<()>>) {
 		std::env::remove_var("WAYLAND_DISPLAY");
 		std::env::set_var("ENABLE_VULKAN_RENDERDOC_CAPTURE", "1");
 	}
-	std::env::set_var(
-		"SMOL_THREADS",
-		std::thread::available_parallelism()
-			.unwrap_or(NonZeroUsize::new(1).unwrap())
-			.get()
-			.to_string(),
-	);
 
 	let init;
 	{
