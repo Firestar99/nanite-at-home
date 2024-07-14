2dcb544d3193252c4c3c8de5aa6783c97b874bef
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-07-14 21:45:52 +0200

    asset-pipeline: fix lantern gltf test

--- space-asset-pipeline/src/meshlet/tests.rs
+++ space-asset-pipeline/src/meshlet/tests.rs
@@ -1,11 +1,17 @@
 use crate::gltf::Gltf;
+use crate::meshlet::merge::{merge_meshlets, MergeStrategy};
 use crate::meshlet::process::process_meshlets;
 use std::path::Path;
 
-const LANTERN_GLTF_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../models/Lantern/glTF/Lantern.gltf");
+const LANTERN_GLTF_PATH: &str = concat!(
+	env!("CARGO_MANIFEST_DIR"),
+	"/../models/models/Lantern/glTF/Lantern.gltf"
+);
 
 #[test]
-fn test_lantern_gltf() {
-	let gltf = Gltf::open(Path::new(LANTERN_GLTF_PATH)).unwrap();
-	let _scene = process_meshlets(&gltf).unwrap();
+fn test_lantern_gltf() -> anyhow::Result<()> {
+	let gltf = Gltf::open(Path::new(LANTERN_GLTF_PATH))?;
+	let scene = process_meshlets(&gltf)?;
+	let _scene = merge_meshlets(scene, MergeStrategy::MergeSingleInstance)?;
+	Ok(())
 }
