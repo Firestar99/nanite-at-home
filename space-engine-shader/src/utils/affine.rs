633446c52529050e34198f20daee8de22065ba18
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-07-22 12:05:33 +0200

    shaders: added Affine3A::transform_*_transposed() functions, more efficient than transpose().transform()

--- space-engine-shader/src/utils/affine.rs
+++ space-engine-shader/src/utils/affine.rs
@@ -1,7 +1,11 @@
-use glam::Affine3A;
+use glam::{Affine3A, Vec3, Vec3A};
 
 pub trait AffineTranspose {
 	fn transpose(&self) -> Self;
+	fn transform_point3_transposed(&self, rhs: Vec3) -> Vec3;
+	fn transform_vector3_transposed(&self, rhs: Vec3) -> Vec3;
+	fn transform_point3a_transposed(&self, rhs: Vec3A) -> Vec3A;
+	fn transform_vector3a_transposed(&self, rhs: Vec3A) -> Vec3A;
 }
 
 impl AffineTranspose for Affine3A {
@@ -16,5 +20,64 @@ impl AffineTranspose for Affine3A {
 		let translation = -(matrix3 * self.translation);
 
 		Self { matrix3, translation }
+	}
+
+	fn transform_point3_transposed(&self, rhs: Vec3) -> Vec3 {
+		let rhs = Vec3A::from(rhs) - self.translation;
+		let mat = self.matrix3.transpose();
+		((mat.x_axis * rhs.x) + (mat.y_axis * rhs.y) + (mat.z_axis * rhs.z)).into()
+	}
+
+	fn transform_vector3_transposed(&self, rhs: Vec3) -> Vec3 {
+		let mat = self.matrix3.transpose();
+		((mat.x_axis * rhs.x) + (mat.y_axis * rhs.y) + (mat.z_axis * rhs.z)).into()
+	}
+
+	fn transform_point3a_transposed(&self, rhs: Vec3A) -> Vec3A {
+		self.matrix3.transpose() * (rhs - self.translation)
+	}
+
+	fn transform_vector3a_transposed(&self, rhs: Vec3A) -> Vec3A {
+		self.matrix3.transpose() * rhs
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::utils::affine::AffineTranspose;
+	use glam::{Affine3A, Quat, Vec3, Vec3A};
+	use std::f32::consts::PI;
+
+	#[test]
+	fn test_transpose() {
+		let affine = Affine3A::from_rotation_translation(
+			Quat::from_axis_angle(Vec3::new(0., 1., 0.), PI / 2.),
+			Vec3::new(42., 69., -1234.),
+		);
+
+		assert!(
+			affine.abs_diff_eq(affine.transpose().transpose(), 0.01),
+			"double transpose"
+		);
+		assert!(
+			affine.inverse().abs_diff_eq(affine.transpose(), 0.01),
+			"transpose eq inverse"
+		);
+	}
+
+	#[test]
+	fn test_transform_transposed() {
+		let affine = Affine3A::from_rotation_translation(
+			Quat::from_axis_angle(Vec3::new(0., 1., 0.), PI / 2.),
+			Vec3::new(67., 69., -1234.),
+		);
+		let base = Vec3::new(567., -87., -43.);
+		let point = affine.inverse().transform_point3(base);
+		let vector = affine.inverse().transform_vector3(base);
+
+		assert!(point.abs_diff_eq(affine.transform_point3_transposed(base), 0.01));
+		assert!(vector.abs_diff_eq(affine.transform_vector3_transposed(base), 0.01));
+		assert!(Vec3A::from(point).abs_diff_eq(affine.transform_point3a_transposed(base.into()), 0.01));
+		assert!(Vec3A::from(vector).abs_diff_eq(affine.transform_vector3a_transposed(base.into()), 0.01));
 	}
 }
