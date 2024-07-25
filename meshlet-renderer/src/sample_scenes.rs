60b86d836e3421465af8d8a771a1cc9e8730989e
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-07-25 15:14:50 +0200

    ganescom: reference gamescom sample scenes

--- meshlet-renderer/src/sample_scenes.rs
+++ meshlet-renderer/src/sample_scenes.rs
@@ -1,5 +1,16 @@
 use space_asset::meshlet::scene::MeshletSceneFile;
 
 pub fn sample_scenes() -> Vec<MeshletSceneFile<'static>> {
-	Vec::from([models::Lantern::glTF::Lantern])
+	Vec::from([
+		models::local::gamescom::bistro::Bistro,
+		models::local::gamescom::Sponza::glTF::Sponza,
+		models::local::gamescom::San_Miguel::san_miguel,
+		models::local::gamescom::rungholt::rungholt,
+		models::local::gamescom::lost_empire::lost_empire,
+		models::local::gamescom::vokselia_spawn::vokselia_spawn,
+		models::local::gamescom::DamagedHelmet::glTF::DamagedHelmet,
+		models::Lantern::glTF::Lantern,
+		models::local::gamescom::lpshead::head,
+		models::local::gamescom::sibenik::sibenik,
+	])
 }
