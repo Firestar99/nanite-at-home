868db983e5cc23f8f8f6042ce9aff9a18bcdbed1
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-08-07 17:54:43 +0200

    renderer: added sun inclination curve, sun affects ambient light

--- meshlet-renderer/src/main_loop.rs
+++ meshlet-renderer/src/main_loop.rs
@@ -23,6 +23,7 @@ use space_engine_shader::material::radiance::Radiance;
 use space_engine_shader::renderer::camera::Camera;
 use space_engine_shader::renderer::frame_data::FrameData;
 use space_engine_shader::renderer::lighting::sky_shader::preetham_sky;
+use space_engine_shader::utils::animated_segments::{AnimatedSegment, Segment};
 use std::f32::consts::PI;
 use std::sync::mpsc::Receiver;
 use vulkano::shader::ShaderStages;
@@ -153,11 +154,26 @@ pub async fn run(event_loop: EventLoopExecutor, inputs: Receiver<Event<()>>) {
 
 			let sun = {
 				const SUN_MAX_ALTITUDE_DEGREE: f32 = 25.;
-				const SUN_INCLINATION_START: f32 = 0.;
-				const SUN_INCLINATION_SPEED: f32 = 0.05;
+				const SUN_INCLINATION_SPEED: f32 = 0.5;
+				const SUN_INCLINATION_CURVE: AnimatedSegment<f32> = AnimatedSegment::new(&[
+					Segment::new(0., 0.),
+					Segment::new(0.2, 0.05),
+					Segment::new(0.5, 0.1),
+					Segment::new(1., 0.15),
+					Segment::new(2., 0.2),
+					Segment::new(3., 0.25),
+					Segment::new(4., 0.3),
+					Segment::new(4., 0.7),
+					Segment::new(5., 0.75),
+					Segment::new(6., 0.8),
+					Segment::new(7., 0.85),
+					Segment::new(8. - 0.5, 0.9),
+					Segment::new(8. - 0.2, 0.95),
+					Segment::new(8., 1.),
+				]);
 
 				let sun_dir = vec3(0., 1., 0.);
-				let inclination = SUN_INCLINATION_START + SUN_INCLINATION_SPEED * delta_time.since_start;
+				let inclination = SUN_INCLINATION_CURVE.lerp(delta_time.since_start * SUN_INCLINATION_SPEED);
 				let sun_dir = Mat3::from_axis_angle(vec3(1., 0., 0.), inclination * 2. * PI) * sun_dir;
 				let sun_dir =
 					Mat3::from_axis_angle(vec3(0., 0., 1.), f32::to_radians(SUN_MAX_ALTITUDE_DEGREE)) * sun_dir;
@@ -172,12 +188,17 @@ pub async fn run(event_loop: EventLoopExecutor, inputs: Receiver<Event<()>>) {
 				}
 			};
 
+			let ambient_light = {
+				const AMBIENT_STARLIGHT: Vec3 = vec3(105. / 255., 129. / 255., 142. / 255.);
+				Radiance(sun.color.0 * 0.1 + AMBIENT_STARLIGHT * 0.1)
+			};
+
 			FrameData {
 				camera: Camera::new(projection, AffineTransform::new(camera_controls.update(delta_time))),
 				debug_settings: debug_settings_selector.get().into(),
 				viewport_size: out_extent.xy(),
 				sun,
-				ambient_light: Radiance(Vec3::splat(0.1)),
+				ambient_light,
 			}
 		};
 
