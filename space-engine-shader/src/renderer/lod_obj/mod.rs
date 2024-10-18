dc12b3f7e957fe8d920ccb85dd40ea8d910990b8
Firestar99 <31222740+Firestar99@users.noreply.github.com>
2024-10-18 14:37:12 +0200

    renderer: remove unused opaque shaders

--- space-engine-shader/src/renderer/lod_obj/mod.rs
+++
@@ -1,4 +0,0 @@
-//! this mod is kept around for now as otherwise rust-gpu mispiles due to my "fix" to not merge symbols as aggressively
-
-pub mod opaque_model;
-pub mod opaque_shader;
--- space-engine-shader/src/renderer/mod.rs
+++ space-engine-shader/src/renderer/mod.rs
@@ -2,5 +2,4 @@ pub mod allocation_buffer;
 pub mod camera;
 pub mod frame_data;
 pub mod lighting;
-pub mod lod_obj;
 pub mod meshlet;
