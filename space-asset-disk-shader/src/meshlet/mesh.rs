a0495487b1c5250540992f0c39e77f2bc31ea6be
Firestar99 <31222740+Firestar99@users.noreply.github.com>
2024-10-23 15:14:31 +0200

    meshlets: give MeshletOffset more bits

--- space-asset-disk-shader/src/meshlet/mesh.rs
+++ space-asset-disk-shader/src/meshlet/mesh.rs
@@ -8,7 +8,7 @@ pub struct MeshletData {
 	pub draw_vertex_offset: MeshletOffset,
 	pub triangle_offset: MeshletOffset,
 }
-assert_transfer_size!(MeshletData, 2 * 4);
+assert_transfer_size!(MeshletData, 4 * 4);
 
 impl AsRef<MeshletData> for MeshletData {
 	fn as_ref(&self) -> &MeshletData {
