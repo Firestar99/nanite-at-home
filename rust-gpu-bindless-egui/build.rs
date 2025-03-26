6927a6fbf2efd3e95846b2b5a044e55251c24d34
Firestar99 <firestar99@sydow.cloud>
2025-03-26 12:51:26 +0100

    Squashed 'rust-gpu-bindless/' changes from 01be4f2..11b3cf3
    11b3cf3 project: cleanup workspace dependencies
    b05537e project: add MIT or Apache 2.0 dual licence
    d75c8dd project: fix egui shader building
    e636467 project: setup workspace Cargo.toml, dependencies and paths
    9c0bd0d project: move crates into final folders
    
    git-subtree-dir: rust-gpu-bindless
    git-subtree-split: 11b3cf34dea82cb063d84851988f2a1617e5144a

--- rust-gpu-bindless-egui/build.rs
+++
@@ -1,8 +0,0 @@
-use std::error::Error;
-
-use space_engine_shader_builder::shader_symbols_builder_configured_for_space_engine;
-
-fn main() -> Result<(), Box<dyn Error>> {
-	shader_symbols_builder_configured_for_space_engine("rust-gpu-bindless-egui-shaders").build()?;
-	Ok(())
-}
