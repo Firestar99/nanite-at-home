943dfcaeeaff03be077926fcfbb159d654e772fc
Firestar99 <firestar99@sydow.cloud>
2025-04-06 12:45:24 +0200

    WIP something works???

--- space-engine-shader/src/renderer/meshlet/meshlet_select.rs
+++ space-engine-shader/src/renderer/meshlet/meshlet_select.rs
@@ -2,7 +2,7 @@ use crate::renderer::compacting_alloc_buffer::{CompactingAllocBufferReader, Comp
 use crate::renderer::frame_data::FrameData;
 use crate::renderer::lod_selection::LodType;
 use crate::renderer::meshlet::intermediate::{MeshletGroupInstance, MeshletInstance};
-use glam::{Affine3A, UVec3, Vec3A};
+use glam::{Affine3A, UVec3, Vec3, Vec3A};
 use rust_gpu_bindless_macros::{bindless, BufferStruct};
 use rust_gpu_bindless_shaders::descriptor::{Buffer, Descriptors, Strong, TransientDesc};
 use space_asset_shader::meshlet::mesh::MeshletMesh;
@@ -57,15 +57,39 @@ fn cull_meshlet(
 	match frame_data.debug_lod_level.lod_type() {
 		LodType::Nanite => {
 			let instance_transform = scene.instances.access(descriptors).load(instance.instance_id as usize);
-			let transform = |sphere: Sphere, radius: f32| {
-				project_to_screen_area(frame_data, instance_transform.world_from_local.affine, sphere, radius)
-					* (frame_data.camera.viewport_size.y as f32 * 0.5)
-			};
-			let ss_error = transform(m.bounds, m.error);
-			let ss_error_parent = transform(m.parent_bounds, m.parent_error);
-			let error_threshold = frame_data.nanite.error_threshold;
-			let draw = ss_error <= error_threshold && error_threshold < ss_error_parent;
-			!draw
+			// let transform = |sphere: Sphere, radius: f32| {
+			// 	project_to_screen_area(frame_data, instance_transform.world_from_local.affine, sphere, radius)
+			// 		* (frame_data.camera.viewport_size.y as f32)
+			// };
+			// let ss_error = transform(m.bounds, m.error);
+			// let ss_error_parent = transform(m.parent_bounds, m.parent_error);
+			// let error_threshold = frame_data.nanite.error_threshold;
+			// let draw = ss_error <= error_threshold && error_threshold < ss_error_parent;
+			// !draw
+
+			// let world_scale = {
+			// 	// Scaling a sphere turns it into an ellipsoid, to turn it back into a sphere we place a sphere around it.
+			// 	// That is equivalent to multiplying the radius by the axis that is scaled up the most.
+			// 	let mat = instance_transform.transform.affine.matrix3;
+			// 	f32::max(f32::max(mat.x_axis.length(), mat.y_axis.length()), mat.z_axis.length())
+			// };
+			let world_scale = 1.0;
+
+			let lod_is_ok = lod_error_is_imperceptible(
+				frame_data,
+				m.bounds,
+				m.error,
+				instance_transform.transform.affine,
+				world_scale,
+			);
+			let parent_lod_is_ok = lod_error_is_imperceptible(
+				frame_data,
+				m.parent_bounds,
+				m.parent_error,
+				instance_transform.transform.affine,
+				world_scale,
+			);
+			!lod_is_ok || parent_lod_is_ok
 		}
 		LodType::Static => m
 			.lod_level_bitmask
@@ -108,5 +132,41 @@ pub fn project_to_screen_area(frame_data: FrameData, world_from_local: Affine3A,
 	let d = center_world.distance(camera.view_from_world.translation()) - radius;
 	let d = f32::max(d, camera.clip_from_view.to_cols_array_2d()[3][2]);
 	let camera_proj = camera.clip_from_view.to_cols_array_2d()[1][1];
-	error / d * (camera_proj * 0.5)
+	(error / d) * (camera_proj * 0.5)
+}
+
+// https://github.com/zeux/meshoptimizer/blob/1e48e96c7e8059321de492865165e9ef071bffba/demo/nanite.cpp#L115
+fn lod_error_is_imperceptible(
+	frame_data: FrameData,
+	lod_sphere: Sphere,
+	simplification_error: f32,
+	world_from_local: Affine3A,
+	world_scale: f32,
+) -> bool {
+	if !simplification_error.is_finite() {
+		return false;
+	}
+
+	let camera = frame_data.camera;
+	let nanite = frame_data.nanite;
+
+	let sphere_world_space = world_from_local.transform_point3(lod_sphere.center());
+	let radius_world_space = world_scale * lod_sphere.radius() * nanite.bounding_sphere_scale;
+	let error_world_space = world_scale * simplification_error;
+
+	let mut projected_error = error_world_space;
+	// if camera.perspective.to_cols_array_2d()[3][3] != 1.0 {
+	// Perspective
+	let distance_to_closest_point_on_sphere =
+		Vec3::distance(sphere_world_space, camera.transform.translation()) - radius_world_space;
+	let distance_to_closest_point_on_sphere_clamped_to_znear = f32::max(
+		distance_to_closest_point_on_sphere,
+		camera.perspective.to_cols_array_2d()[3][2],
+	);
+	projected_error /= distance_to_closest_point_on_sphere_clamped_to_znear;
+	// }
+	projected_error *= camera.perspective.to_cols_array_2d()[1][1] * 0.5;
+	projected_error *= camera.viewport_size.x as f32;
+
+	projected_error < nanite.error_threshold
 }
